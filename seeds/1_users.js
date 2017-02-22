4d16ca24d3bbb37c8aa4e9595503a81607e2ebac
Jamieson Bates <jamiesonbates@gmail.com>
2017-02-22 13:48:01 -0700

    Add additional user to users seed file for verification purposes

--- seeds/1_users.js
+++ seeds/1_users.js
@@ -10,6 +10,13 @@ exports.seed = function(knex) {
           email: 'jamiesonbates@gmail.com',
           h_pw: 'password',
           address: '106 Bellevue Ave E, Apt 203, Seattle, WA, 98102'
+        },
+        {
+          id: 2,
+          username: 'minhchau',
+          email: 'xiu.chau@gmail.com',
+          h_pw: 'password',
+          address: '100 Jackson Street, Seattle, WA, 98102'
         }
       ]);
     });
