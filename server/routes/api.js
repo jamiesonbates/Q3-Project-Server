68024e544971a3b0cbe204b8e8032979562f8828
Jamieson Bates <jamiesonbates@gmail.com>
2017-02-27 14:46:03 -0800

    Prepare the Heroku

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const boom = require('boom');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt-as-promised');
 const jwt = require('jsonwebtoken');
 const knex = require('../../knex');
 const router = require('express').Router();
@@ -21,9 +21,9 @@ router.get('/', (req, res) => {
 router.post('/users', (req, res, next) => {
   const { username, email, password, img_url, address } = req.body;
 
-  bcyrpt.genSalt(12, (err, salt) => {
-    bcrypt.hash(password, salt, (err, hash) => {
-      return knex('users').insert({ username: username, email: email, h_pw: hash, img_url: img_url, address: address }, '*');
+  bcrypt.hash(password, 12)
+    .then((h_pw) => {
+      return knex('users').insert({ username: username, email: email, h_pw: h_pw, img_url: img_url, address: address }, '*');
     })
     .then((users) => {
       const user = users[0];
@@ -35,7 +35,6 @@ router.post('/users', (req, res, next) => {
     .catch((err) => {
       next(err);
     });
-  })
 });
 
 router.post('/token', (req, res, next) => {
@@ -55,25 +54,26 @@ router.post('/token', (req, res, next) => {
       user = row;
       console.log(user);
 
-      return bcrypt.compare(password, hash, (err, res) => {
-        if (!res) {
-          throw boom.create(400, 'Bad email or password');
-        }
+      return bcrypt.compare(password, user.h_pw);
+    })
+    .then(() => {
+      const claim = { userId: user.id };
+      const token = jwt.sign(claim, process.env.JWT_KEY, {
+        expiresIn: '30 days'
+      });
 
-        const claim = { userId: user.id };
-        const token = jwt.sign(claim, process.env.JWT_KEY, {
-          expiresIn: '30 days'
-        });
+      res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+        secure: router.get('env') === 'production'
+      });
 
-        res.cookie('token', token, {
-          httpOnly: true,
-          expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
-          secure: router.get('env') === 'production'
-        });
+      delete user.h_pw;
 
-        delete user.h_pw;
-
-        res.send(user);
+      res.send(user);
+    })
+    .catch(bcrypt.MISMATCH_ERROR, () => {
+      throw boom.create(400, 'Bad email or password');
     })
     .catch((err) => {
       next(err);
